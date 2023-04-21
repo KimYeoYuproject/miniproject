7075a054d71983ead4e7785660a73ecc419d0c9d
miji <mjyeo6031@gmail.com>
2023-04-21 17:05:21 +0900

    test

--- src/test/test.java
+++ src/test/test.java
@@ -5,6 +5,7 @@ public class test {
         System.out.println("dff");
         System.out.println();
         System.out.println("하이");
+        System.out.println("민지");
         System.out.println("미지테스트");
     }
 }
