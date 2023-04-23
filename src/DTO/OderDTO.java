24c423f4ad130f575be0d77a8633d9dfb3ba7948
miji6031 <mjyeo6031@gmail.com>
2023-04-24 01:15:08 +0900

    Order파트 초기구성

---
+++ src/DTO/OderDTO.java
@@ -0,0 +1,22 @@
+package DTO;
+
+import Model.OderVO;
+
+/**
+ * @author miji
+ * 저장된 주문정보 데이터를 저장, 조회, 수정, 삭제가능한 객체
+ * 주문 정보는 주문번호와 주문자별로 조회가능하다.
+ * 주문 정보 전체조회가 가능하며 월,년 별로 오름차순/ 내림차순으로 조회 가능하다.
+ */
+public class OderDTO {
+	
+	// 주문내역을 입력받아 배열에 저장
+	OderVO[] orderlist = new OderVO[10];
+	
+	
+	
+	
+	
+	
+
+}
