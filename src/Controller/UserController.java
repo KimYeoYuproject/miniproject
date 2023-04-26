c52910c68c223442e720183feae50cc38a9be66e
yoosc89 <yoosc89@gmail.com>
2023-04-26 22:29:43 +0900

    ..

---
+++ src/Controller/UserController.java
@@ -0,0 +1,13 @@
+package Controller;
+
+public class UserController {
+
+    // UserDTO 필드 변수
+
+    // 유저 추가, UserDTO 이용
+
+    // 유저 찾는 메소드, 핸드폰 번호 입력 받아 UserVO 객체 리턴, UserDTO 이용
+
+    // UserVO 모두 반환하는 메소드, UserVO 객체 배열 리리턴, UserDTO 이용
+
+}
---
+++ src/DTO/UserDTO.java
@@ -0,0 +1,13 @@
+package DTO;
+
+public class UserDTO {
+
+    // UserVO 객체 배열 필드 변수
+
+    // UserVO 객체 배열에 추가하는 메소드
+
+    // 휴대폰 번호를 매개변수로 받아 해당 UserVO 객체 리턴
+
+    // UserVO 객체 배열 모두 반환
+
+}
